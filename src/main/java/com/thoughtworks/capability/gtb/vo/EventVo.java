f8c224426c39fe4585099e1ee64c115e2c9768e8
hejie <565703319@qq.com>
2020-11-05 22:57:44 +0800

    [hejie] add time serializer

--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.thoughtworks.capability.gtb.util.TimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,7 +18,7 @@ public class EventVo {
   private String id;
   private String name;
   private EventType type;
-  @JsonFormat(pattern = "")
+  @JsonSerialize(using = TimeSerializer.class)
   private Date time;
   @JsonUnwrapped
   private UserVo user;
